affce382bd1ea7efda2eff05c8c999db1c639761
Bavon101 <bavonfate@gmail.com>
2022-04-11 15:39:55 +0300

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import DetailsPage from './pages/DetailsPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      Pixels
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/details" element={<DetailsPage />} />
+      </Routes>
     </div>
   );
 }
