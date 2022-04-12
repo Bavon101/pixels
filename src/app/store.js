0f1566a11e0d6b37a599f64433122d15019007f0
Bavon101 <bavonfate@gmail.com>
2022-04-12 16:04:20 +0300

    Add rootReducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,11 @@
 import { applyMiddleware, combineReducers, createStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
-import imagesReducer from '../redux/images';
+import { imageReducer, imagesReducer } from '../redux/images';
 
 const rootReducer = combineReducers(
   {
     imagesReducer,
+    imageReducer,
   },
 );
 const store = createStore(rootReducer, applyMiddleware(thunk));
