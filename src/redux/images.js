8f7682df183a6835396eb2f9f09760c01741bad2
Bavon101 <bavonfate@gmail.com>
2022-04-12 16:05:57 +0300

    Add images reducer

---
+++ src/redux/images.js
@@ -0,0 +1,62 @@
+import {
+  FETCH_DATA_BEGIN, FETCH_DATA_FAILURE, FETCH_DATA_SUCCESS,
+  IMAGE_FETCH_BEGIN, IMAGE_FETCH_FAILURE, IMAGE_FETCH_SUCCESS,
+} from '../api/data_fetch_statuses';
+
+export function imagesReducer(state = {
+  images: [],
+  loading: false,
+  error: null,
+}, action) {
+  switch (action.type) {
+    case FETCH_DATA_BEGIN:
+
+      return {
+        ...state,
+        loading: true,
+      };
+    case FETCH_DATA_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        images: [
+          ...action.payload,
+        ],
+      };
+    case FETCH_DATA_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+export function imageReducer(state = {
+  loading: false,
+  image: null,
+  error: null,
+}, action) {
+  switch (action.type) {
+    case IMAGE_FETCH_BEGIN:
+
+      return {
+        ...state,
+        loading: true,
+      };
+    case IMAGE_FETCH_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case IMAGE_FETCH_SUCCESS:
+      return {
+        ...state,
+        image: action.payload.hits[0],
+      };
+    default:
+      return state;
+  }
+}
