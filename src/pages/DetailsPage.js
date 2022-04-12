bdeef3fbe369841dd078e791f9f4a9d39dd97168
Bavon101 <bavonfate@gmail.com>
2022-04-12 16:05:35 +0300

    Add dummy images card to home page and details navigation

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 export default function DetailsPage() {
+  const { search } = useLocation();
+  const id = new URLSearchParams(search).get('id');
+  console.log(id);
+
   return (
     <div>DetailsPage</div>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import { Grid } from '@mui/material';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import fetchImages from '../api/data_fetch';
+import ImageCard from '../components/image/ImageCard';
 
 export default function HomePage() {
+  const imagesData = useSelector((state) => state.imagesReduceer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (imagesData.images.length === 0) {
+      dispatch(fetchImages());
+    }
+  }, []);
   return (
-    <div>HomePage</div>
+    <div>
+      <ImageCard />
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={6}>
+          <ImageCard />
+        </Grid>
+        <Grid item xs={6}>
+          <ImageCard />
+        </Grid>
+        <Grid item xs={6}>
+          <ImageCard />
+        </Grid>
+        <Grid item xs={6}>
+          <ImageCard />
+        </Grid>
+        <Grid item xs={6}>
+          <ImageCard />
+        </Grid>
+        <Grid item xs={6}>
+          <ImageCard />
+        </Grid>
+      </Grid>
+    </div>
   );
 }
