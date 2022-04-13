5265516ad352ba3445146c5155b398024e09b712
Bavon101 <bavonfate@gmail.com>
2022-04-13 13:01:33 +0300

    Add appbar and app styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import NavgationBar from './components/NavgationBar';
 import DetailsPage from './pages/DetailsPage';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
+      <NavgationBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/details" element={<DetailsPage />} />
